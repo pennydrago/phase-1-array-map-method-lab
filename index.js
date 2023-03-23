e733fd64fe81e4f66eb2142eac65c06b7194d63e
Penelope Drago <prd@drago.com.au>
2023-03-23 14:00:39 +1100

    add functions

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+
+// SOLUTION
+
+// Note: Resource used to help with solution: 'Three Ways to Title Case a Sentence in JavaScript' by Sonya Moisset
+// https://www.freecodecamp.org/news/three-ways-to-title-case-a-sentence-in-javascript-676a9175eb27/
+
+// STEP 1: Function to title case a string, by combining the following steps
+
+// 1. Split each title (a string) in the tutorials list into an array of strings to iterate over each word, with a space as the separator
+// .split(' ') // e.g. title = ["what", "does" ...]
+
+// 2. Map over each title (i.e. an array of strings) with an anonymous function as the callback function
+// .map(function(word) {
+//   return (word.slice(0,1).toUpperCase() + word.slice(1));
+// })
+
+// 3. Join each word to reform the title with a space in between each word
+// .join(' ')
+
+function upperCaseEachWord(title) {
+  return title.split(' ').map(function(word) {
+    return (word.slice(0,1).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
+
+// STEP 2: Function to apply title case function to each element in the tutorials array by using .map() method
+
+function titleCased() {
+  return tutorials.map(upperCaseEachWord);
+}
+
+// STEP 3: Console log output of titleCased function, to test solution
+
+console.log(titleCased());
